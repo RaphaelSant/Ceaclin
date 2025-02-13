3b961808bca23e0740a8b016f4c130c07b83e27d
RaphaelSant <raphael.sant.emp@gmail.com>
2025-02-13 18:06:13 -0400

    Navbar atualizada

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,23 +1,28 @@
-// Navbar
 import React from "react";
 import Logo from "./logo_nav.png";
-import styles from './navbar.module.css';  // Importe o arquivo CSS
-
+import styles from './navbar.module.css';
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ height: '80px' }}>
             <div className="container d-flex justify-content-between">
                 {/* Imagem da logo */}
-                <img src={Logo} className="me-3 img-fluid" alt="Logo" style={{ maxHeight: '30px' }} />
+                <Link to="/">
+                    <img src={Logo} className="me-3 img-fluid" alt="Logo" style={{ maxHeight: '30px' }} />
+                </Link>
 
-                {/* Botão de Resultados visível em mobile à esquerda e desktop à direita */}
-                <a href="https://resultados.ceaclin.com.br/matrixnet/wfrmLogin.aspx" target="_blank" rel="noopener noreferrer" className={`${styles.resultadosBtn} d-flex d-lg-none`}>
+                {/* Botão de Resultados visível em mobile */}
+                <a href="https://resultados.ceaclin.com.br/matrixnet/wfrmLogin.aspx"
+                    target="_blank" rel="noopener noreferrer"
+                    className={`${styles.resultadosBtn} d-flex d-lg-none`}>
                     Resultados
                 </a>
 
-                {/* Botão de menu (hambúrguer) - visível em mobile */}
-                <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                {/* Botão de menu (hambúrguer) */}
+                <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
@@ -25,32 +30,24 @@ export default function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="https://www.google.com">Home</a>
+                            <a className={`nav-link ${styles.navLinkCustom}`} href="#">Exames</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="https://www.google.com">Link</a>
+                            <a className={`nav-link ${styles.navLinkCustom}`} href="#">Perguntas Frequentes</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className={`nav-link dropdown-toggle`} href="https://www.google.com" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Sobre Nós
-                            </a>
-                            <ul className={`${styles.dropdownMenu} dropdown-menu`}>
-                                <li><a className={`${styles.dropdownItem} dropdown-item`} href="https://www.google.com">Sobre Nós</a></li>
-                                <li><a className={`${styles.dropdownItem} dropdown-item`} href="https://www.google.com">Nossas Unidades</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className={`${styles.dropdownItem} dropdown-item`} href="https://www.google.com">Fale Conosco</a></li>
-                            </ul>
+                        <li className="nav-item">
+                            <a className={`nav-link ${styles.navLinkCustom}`} href="#">Sobre Nós</a>
                         </li>
                     </ul>
                 </div>
 
-                {/* Botão de Resultados visível apenas em desktop à direita */}
-                <a href="https://resultados.ceaclin.com.br/matrixnet/wfrmLogin.aspx" target="_blank" rel="noopener noreferrer" className={`${styles.resultadosBtn} d-none d-lg-flex ms-auto`}>
+                {/* Botão de Resultados visível apenas em desktop */}
+                <a href="https://resultados.ceaclin.com.br/matrixnet/wfrmLogin.aspx"
+                    target="_blank" rel="noopener noreferrer"
+                    className={`${styles.resultadosBtn} d-none d-lg-flex ms-auto`}>
                     Resultados
                 </a>
-
             </div>
         </nav>
-
     );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,8 +5,120 @@ import Navbar from "../../components/navbar";
 export default function Home() {
     return (
         <>
-         <Navbar />
-         <h1>Esta e a homePage</h1>
+            <Navbar />
+            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-bs-interval="2000">
+                        <img src="https://img.freepik.com/vetores-gratis/fundo-de-curva-azul_53876-113112.jpg" className="d-block w-100" style={{ maxHeight: '500px' }} alt="..." />
+                    </div>
+                    <div className="carousel-item" data-bs-interval="2000">
+                        <img src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?cs=srgb&dl=pexels-padrinan-255379.jpg&fm=jpg" style={{ maxHeight: '500px' }} className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item" data-bs-interval="2000">
+                        <img src="https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg" style={{ maxHeight: '500px' }} className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Anterior</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Próximo</span>
+                </button>
+            </div>
+
+            <div className="conteiner mt-3">
+                <h1 className="text-center">Nossos Serviços</h1>
+
+                <div className="d-flex justify-content-center align-items-center gap-4">
+
+                    <div class="card" style={{ width: '18rem' }}>
+                        <img src="https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    <div class="card" style={{ width: '18rem' }}>
+                        <img src="https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    <div class="card" style={{ width: '18rem' }}>
+                        <img src="https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="conteiner mt-3">
+                <h1 className="text-center">Agende Exame Sem Sair de Casa</h1>
+
+                <div className="d-flex justify-content-center align-items-center gap-4">
+
+                    <p>Agora ficou ainda mais fácil cuidar da sua saúde. Agende seu exame online, de forma prática e rápida, sem sair do conforto da sua casa.</p>
+
+                    <a href="#" class="btn btn-primary">Saiba Mais</a>
+
+                </div>
+            </div>
+
+            <div className="conteiner mt-3">
+                <h1 className="text-center">Conforto e Qualidade</h1>
+
+                <div className="d-flex justify-content-center align-items-center gap-4">
+
+                    <p>No Laboratório São Rafael, cada detalhe foi pensado para oferecer a melhor experiência aos nossos pacientes. Desde ambientes acolhedores até a agilidade no atendimento, priorizamos o seu conforto e bem-estar.</p>
+
+                    <ul>
+                        <li>Cuidado acolhedor para todas as idades.</li>
+                        <li>Espaços adaptados e conforto garantido.</li>
+                        <li>Exames com atenção especial para idosos.</li>
+                    </ul>
+
+                    <a href="#" class="btn btn-primary">Agende agora mesmo</a>
+
+                </div>
+            </div>
+
+            <div className="conteiner mt-3">
+                <h1 className="text-center">Nossas Unidades</h1>
+
+                <div className="d-flex justify-content-center align-items-center gap-4">
+
+                    <p>No Laboratório São Rafael, cada detalhe foi pensado para oferecer a melhor experiência aos nossos pacientes. Desde ambientes acolhedores até a agilidade no atendimento, priorizamos o seu conforto e bem-estar.</p>
+
+                    <a href="#" class="btn btn-primary">Agende agora mesmo</a>
+
+                </div>
+            </div>
+
+            <div className="conteiner mt-3">
+                <h1 className="text-center">Uma empresa amiga e certificada:</h1>
+
+                <div className="d-flex justify-content-center align-items-center gap-4">
+
+                    <img src="https://tecnotron.com.br/wp-content/uploads/2024/07/pngwing.com-11.png.webp" style={{ height: '150px' }} />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFy_l6QfBomRB3ooQIhmQAnFCjMe-c726ONw&s" style={{ height: '150px' }} />
+
+                    <p>No Laboratório São Rafael, cada detalhe foi pensado para oferecer a melhor experiência aos nossos pacientes. Desde ambientes acolhedores até a agilidade no atendimento, priorizamos o seu conforto e bem-estar.</p>
+
+                    <a href="#" class="btn btn-primary">Agende agora mesmo</a>
+
+                </div>
+            </div>
         </>
     );
 }
